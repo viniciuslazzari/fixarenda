5c26f39cbfd419a4e103f73c73b7710cd7f64a8b
Vinicius Lazzari <vinilazzari028@gmail.com>
2024-02-07 19:02:52 -0300

    🚧 WIP

--- services/data.ts
+++ services/data.ts
@@ -1,20 +1,22 @@
 import axios from "axios";
 
 export async function getIpca(months: number) {
-  const IPCA_API_URL = "https://ipeadata.gov.br/api/odata4/ValoresSerie(SERCODIGO='PRECOS12_IPCAG12')";
+  const IPCA_API_URL = "https://api.bcb.gov.br/dados/serie/bcdata.sgs.10844/dados?formato=json";
 
   const response: any = await axios.get(IPCA_API_URL)
 
-  const raw: number = response.data.value
+  const raw: number = response.data
     .slice(-months)
-    .map((item: any) => 1 + (item.VALVALOR / 100))
-    .reduce((a: number, b: number) => a * b, 1)
+    .map((item: any) => item.valor)
+    .reduce((a: number, b: number) => a + b, 0);
 
-  return Math.round(((raw - 1) * 100) * 100) / 100;
+  console.log(response.data.slice(-months).map((item: any) => item.valor))
+
+  return Math.round((raw * 100) * 100) / 100;
 }
 
 export async function getCdi(months: number) {
-  const CDI_API_URL = "https://ipeadata.gov.br/api/odata4/ValoresSerie(SERCODIGO='BM12_TJCDI12')";
+  const CDI_API_URL = "http://ipeadata.gov.br/api/odata4/ValoresSerie(SERCODIGO='BM12_TJCDI12')";
 
   const response: any = await axios.get(CDI_API_URL)
 
